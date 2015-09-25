56dea1c487f6cfaed77dd34fb628ec5abeabc870
unknown <jvaccaro@1H8YLX1.olin.edu>
2015-09-25 16:04:57 -0400

    HW 2 is complete

--- HW/HW2/hw2.do
+++ HW/HW2/hw2.do
@@ -1,12 +1,10 @@
 vlog -reportprogress 300 -work work multiplexer.v decoder.v adder.v
-vsim -voptargs="+acc" testDecoder
+vsim -voptargs="+acc" testFullAdder
 add wave -position insertpoint  \
-sim:/testDecoder/addr0 \
-sim:/testDecoder/addr1 \
-sim:/testDecoder/enable \
-sim:/testDecoder/out0 \
-sim:/testDecoder/out1 \
-sim:/testDecoder/out2 \
-sim:/testDecoder/out3
+sim:/testFullAdder/a \
+sim:/testFullAdder/b \
+sim:/testFullAdder/carryin \
+sim:/testFullAdder/carryout \
+sim:/testFullAdder/out
 run -all
 wave zoom full
--- HW/HW2/multiplexer.v
+++ HW/HW2/multiplexer.v
@@ -11,10 +11,67 @@ module structuralMultiplexer(out, address0,address1, in0,in1,in2,in3);
 output out;
 input address0, address1;
 input in0, in1, in2, in3;
-  // Your multiplexer code here
+
+wire naddress0, naddress1;
+wire beg0, beg1, beg2, beg3;
+wire mid0, mid1, mid2, mid3;
+wire c01, c23;
+
+//Define gates with delays
+`define And and #50
+`define Or or #50
+`define Nand nand #50
+`define Not not #50
+`define Xor xor #50
+`define Nor nor #50
+
+`Not inv0(naddress0, address0);
+`Not inv1(naddress1, address1);
+`And ag0(beg0, naddress0, naddress1);
+`And ag1(beg1, address0, naddress1);
+`And ag2(beg2, naddress0, address1);
+`And ag3(beg3, address0, address1);
+`And if0(mid0, beg0, in0);
+`And if1(mid1, beg1, in1);
+`And if2(mid2, beg2, in2);
+`And if3(mid3, beg3, in3);
+`Or cc01(c01, mid0, mid1);
+`Or cc23(c23, mid2, mid3);
+`Or cc0123(out, c01, c23);
 endmodule
 
 
 module testMultiplexer;
-  // Your test code here
+reg address0, address1, in0, in1, in2, in3;
+wire out;
+//behavioralMultiplexer multi(out, address0,address1, in0,in1,in2,in3);
+structuralMultiplexer yolo(out, address0,address1, in0,in1,in2,in3);
+initial begin
+$display("Structural Multiplexer");
+$display("A0 A1| in0 in1 in2 in3 || Out | Expected Output");
+address0=0;address1=0;in0=0;in1=0;in2=0;in3=0; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | False", address0, address1, in0, in1, in2, in3, out);
+address0=0;address1=0;in0=1;in1=0;in2=0;in3=0; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+address0=0;address1=0;in0=1;in1=1;in2=1;in3=1; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+address0=1;address1=0;in0=0;in1=0;in2=0;in3=0; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | False", address0, address1, in0, in1, in2, in3, out);
+address0=1;address1=0;in0=0;in1=1;in2=0;in3=0; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+address0=1;address1=0;in0=1;in1=1;in2=1;in3=1; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+address0=0;address1=1;in0=0;in1=0;in2=0;in3=0; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | False", address0, address1, in0, in1, in2, in3, out);
+address0=0;address1=1;in0=0;in1=0;in2=1;in3=0; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+address0=0;address1=1;in0=1;in1=1;in2=1;in3=1; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+address0=1;address1=1;in0=0;in1=0;in2=0;in3=0; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | False", address0, address1, in0, in1, in2, in3, out);
+address0=1;address1=1;in0=0;in1=0;in2=0;in3=1; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+address0=1;address1=1;in0=1;in1=1;in2=1;in3=1; #1000 
+$display("%b  %b|  %b  %b  %b  %b || %b | True", address0, address1, in0, in1, in2, in3, out);
+end
 endmodule
